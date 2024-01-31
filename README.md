688195df8d03eaa29bb6991e84d9c4fe3697248e
ngmn31 <rotten.dignity@gmail.com>
2024-01-31 14:09:58 +0900

    add mongodb

---
+++ README.md
@@ -0,0 +1,5 @@
+提出用フォルダ
+
+insertMany.jsのパス:myapp/node_modules/
+deleteMany.jsのパス:myapp/node_modules/
+note.js
--- myapp/routes/notes.js
+++ myapp/routes/notes.js
@@ -4,7 +4,7 @@ var router = express.Router();
 
 // 接続情報を設定
 const { MongoClient } = require("mongodb");
-const uri = "****************";
+const uri = "*****";
 const client = new MongoClient(uri);
 
 
@@ -13,9 +13,8 @@ router.get('/', async (req, res) => {
 const database = client.db('notes');
 const notes = database.collection('notes');
 
-// idが１のドキュメントを取得
-const query = { id: 2 };
-const note = await notes.findOne(query);
+
+const note = await notes.find({}).toArray();
 
 res.json(note);
 })
